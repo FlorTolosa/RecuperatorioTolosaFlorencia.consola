fc6299a1c250773008207a219fd5ed3a00c834ed
FlorTolosa <flo89t@gmail.com>
2024-08-26 21:52:24 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio01.consola/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.Design;
+
+namespace Ejercicio01.consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ingrese la base del paralelogramo: ");
+                var Base = double.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese la altura del paralelogramo: ");
+                var altura = double.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el lado del paralelogramo: ");
+                var lado = double.Parse(Console.ReadLine());
+
+                if (Base > 0)
+                {
+                    var perimetro = 2 * (Base + lado);
+                    var superficie = Base * altura;
+                    Console.WriteLine($"Paralelogramo de base {Base} , altura {altura} , y lado {lado} tiene una superficie de {superficie} y un perimetro de {perimetro}.");
+
+                }
+
+                if (altura > 0)
+                    {
+                        var perimetro = 2 * (Base + lado);
+                        var superficie = Base * altura;
+                    Console.WriteLine($"Paralelogramo de base {Base} , altura {altura} , y lado {lado} tiene una superficie de {superficie} y un perimetro de {perimetro}.");
+
+                }
+
+                if    (lado > 0)
+                    {
+                    var perimetro = 2 * (Base + lado);
+                    var superficie = Base * altura;
+                    Console.WriteLine($"Paralelogramo de base {Base} , altura {altura} , y lado {lado} tiene una superficie de {superficie} y un perimetro de {perimetro}.");
+                }
+                        else
+                       Console.WriteLine("Se deben ingresar un numeros mayores a cero!!!");
+                
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Algo salió mal!!!");
+            }
+        }
+    }
+}
---
+++ Ejercicio02.consola/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Ejercicio02.consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double mayorSuperficie = 0;
+            int interaccion = 0;
+            double promediosuperficie = 0;
+            int Base;
+            int Altura;
+            int Lado, perimetro, superficie;
+            for (int i = 0; i < 5; i++)
+            {
+
+                do
+                {
+                    Console.Write("Ingrese la base del paralelogramo: ");
+
+                    Base = int.Parse(Console.ReadLine());
+
+                    if (Base > 0)
+                        break;
+                    else
+                    {
+                        Console.WriteLine("Base mal ingresada!!!");
+
+                    }
+                }
+                while (true);
+                do
+                {
+                    Console.Write("Ingrese la altura del paralelogramo: ");
+                    Altura = int.Parse(Console.ReadLine());
+                    if (Altura > 0)
+                        break;
+                    Console.WriteLine("Altura mal ingresada!!!");
+
+                } while (true);
+                do
+                {
+                    Console.Write("Ingrese el lado del paralelogramo: ");
+                    Lado = int.Parse(Console.ReadLine());
+                    if (Lado > 0)
+                        break;
+                    Console.WriteLine("Lado mal ingresado!!!");
+                }
+                while (true);
+                perimetro = 2 * (Base + Lado);
+                superficie = Base * Altura;
+                Console.WriteLine($"Paralelogramo de base {Base} , altura {Altura} , y lado {Lado} tiene una superficie de {superficie} y un perimetro de {perimetro}.");
+
+                promediosuperficie = superficie / 5;
+                if {
+                    mayorSuperficie > promediosuperficie; 
+                    
+
+
+
+            }
+        }
+    }
+}
+
